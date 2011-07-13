b1228c41f95edaf30bf49f232213a882fac4d322
Kelvin Wong <kelvin@insights.net.au>
2011-07-13 14:13:39 +1000

    use drush to download and install drupal, and download all required modules
    Signed-off-by: Kelvin Wong <kelvin@insights.net.au>

--- INprofile.sh
+++ INprofile.sh
@@ -7,54 +7,34 @@
 DRUPAL=drupal-7.4
 BASEPATH=www
 
-#Just for local testing
-#alias drush='/Users/kelvin/Sites/INprofile/drush/drush'
-
-
-createDirCase () {
-	case $createDir in
-	y)
-	echo "copy default"
-	;;
-	
-	n) #create settings.php and files directory
-	
-		
-	
-		SITEDIR = $BASEPATH/sites/default
-		cp $SITEDIR/default.settings.php $SITEDIR/settings.php
-		#mkdir $SITEDIR/files
-		
-		#chmod 777 $SITEDIR/settings.php
-		#chmod 777 $SITEDIR/files	
-		./$BASEPATH/drush dd 
-	;;
-	*)
-	echo "Please use y or n"
-	read createDir
-	createDirCase
-	;;
-	esac
-} 
-
 #Download and install Drush
 wget http://ftp.drupal.org/files/projects/drush-7.x-4.4.tar.gz
 tar -xzf drush-7.x-4.4.tar.gz
 rm -f drush-7.x-4.4.tar.gz
 echo "alias drush='php ~/drush/drush.php'" >> ~/.bash_profile
 source .bash_profile
-drush status
 
 #Download Drupal
+drush dl $DRUPAL
+cp -pRa $DRUPAL/. $BASEPATH/
+rm -rf $DRUPAL
+cd $BASEPATH
 
-#drush dl $DRUPAL
-#cp -pRa $DRUPAL/. $BASEPATH/
-#rm -rf $DRUPAL
+
+#Install Drupal
+echo "Drupal INstallation"
+echo "Please enter the site directory: "
+read site_dir
+echo "Please enter the database name:"
+read db_name
+echo "Please enter the datebase username:"
+read db_user
+echo "Please enter the datebase password:"
+read db_pwd
+
+drush site-install standard --sites-subdir=$site_dir --db-url=mysql://$db_user:$db_pwd@localhost/$db_name
 
 
-#Drupal Installation
-#echo "Create site directory from default? (y/n)"
-#read createDir
-#createDirCase
+#Contribution modules
+drush dl admin_menu-7.x-3.x-dev ctools-7.x-1.x-dev devel-7.x-1.x-dev zenophile-7.x-1.0 custom_formatters-7.x-2.x-dev filefield_sources-7.x-1.4 imce-7.x-1.4 imce_crop-7.x-1.x-dev imce_mkdir-7.x-1.x-dev block_titlelink-7.x-1.x-dev diff-7.x-2.x-dev globalredirect-7.x-1.x-dev iconizer-7.x-1.1 image_caption-7.x-1.0-beta3 invisimail-7.x-1.x-dev masquerade-7.x-1.x-dev nodeblock-7.x-1.1 pathauto-7.x-1.x-dev token-7.x-1.0-beta2 page_title-7.x-2.x-dev google_analytics-7.x-1.x-dev extlink-7.x-1.12 imce_wysiwyg-7.x-1.x-dev wysiwyg-7.x-2.1 views-7.x-3.0-rc1 xmlsitemap-7.x-2.0-beta3 zen-7.x-3.x-dev
 
-#Download 
