73eed856524cd4d9f21e166269a92d198fe29688
Kelvin Wong <kelvin@insights.net.au>
2011-07-28 12:50:08 +1000

    tinymce error fix
    Signed-off-by: Kelvin Wong <kelvin@insights.net.au>

--- INprofile.sh
+++ INprofile.sh
@@ -27,7 +27,7 @@ echo '#Download Tinymce'
 mkdir sites/all/libraries
 wget --no-check-certificate http://tinymce.moxiecode.com/track.php?url=http%3A%2F%2Fgithub.com%2Fdownloads%2Ftinymce%2Ftinymce%2Ftinymce_3.4.3.2.zip
 unzip tinymce_3.4.3.2.zip
-mv tinymce sites//all//libraries/
+mv tinymce sites/all/libraries/
 rm -f tinymce_3.4.3.2.zip
 
 #Install Drupal
